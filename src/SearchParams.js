bd3abfa4ebf745296d513ac9d97a8eed7b7f7ae4
Jeff Li <jeff.li@gocanvas.com>
2019-07-08 19:55:36 -0400

    Created async function for requestPets on form submit. updated package.json wtih browser list for async support

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,6 +7,17 @@ const SearchParams = function(){
 	const [breeds, setBreeds] = useState([]);
 	const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
 	const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+	const [pets, setPets] = useState([]);
+
+	async function requestPets(){
+		const { animals } = await pet.animals({
+			location,
+			breed,
+			type: animal
+		})
+
+		setPets(animals || []);
+	}
 
 	useEffect(() => {
 		setBreeds([]);
@@ -21,7 +32,10 @@ const SearchParams = function(){
 	return (
 		<div className='search-params'>
 		<h1>{location}</h1>
-			<form>
+			<form onSubmit={(e) => {
+				e.preventDefault();
+				requestPets();
+			}}>
 				<label htmlFor='location'>
 					Location
 					<input id='location' value={location} placeholder='location' onChange={e => setLocation(e.target.value)}/>
